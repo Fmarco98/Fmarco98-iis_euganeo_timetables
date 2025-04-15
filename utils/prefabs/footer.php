d7d299513d486c3aa00de6c22bb63714a0b95c79
ytc_fmarco98 <fuck@you.wtf>
2025-04-15 19:57:26 +0200

    user guide file

--- utils/prefabs/footer.php
+++ utils/prefabs/footer.php
@@ -3,11 +3,16 @@
         <footer>
             <div class="container">  
                 <img src="<?php echo $folder_prefix ?>imgs/logo/logo_iiseuganeo.png" alt="iiS Euganeo timetables" id="logoname">
+                <div id="content">
+                    <div>
+                        <h3>Bisogni di aiuto?</h3>
+                        <p>Scarica il <a href="<?php echo $folder_prefix ?>files/manuale.pdf" target="_blank">manuale</a></p>
+                    </div>
+                </div>
             </div>
-            
             <div class="container" style="min-height: auto;">
                 <p>Tutti i diritti riservati 2025™</p>
-                <p style="margin-left: auto; font-style: oblique">Creato da: Casciello Marco, Colturato Davide e Mattiolo Luca</p>
+                <p id="credits">Creato da: Casciello Marco, Colturato Davide e Mattiolo Luca</p>
             </div>
         </footer>
 <?php } ?>
